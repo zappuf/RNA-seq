a5291c85a3dd63f66e9b84472a2137c8e7621865
frz02001 <frank.zappulla@uconn.edu>
2020-11-30 21:37:53 -0500

    pseudo counts dynamically assigned for each gene

--- 4_log_ratio_from_all.py
+++ 4_log_ratio_from_all.py
@@ -6,6 +6,7 @@ from util import convert_gene
 from config import study_dir, results_dir
 
 pseudo_count = 0.01
+dynamic_pseudo_count = True
 
 F_KO = "F_KO_grouped.sf"
 F_WT = "F_WT_grouped.sf"
@@ -19,17 +20,57 @@ WT_FM = "WT_FM_filtered_normalized.sf"
 
 
 def get_min(list1):
-    min = list1[0] if list1[0] > 0.0 else float("inf")
-    for elem in list1:
-        if elem < min and elem > 0.0:
-            min = elem
+    try:
+        min = list1[0] if list1[0] > 0.0 else float("inf")
+    except Exception as e:
+        print(f"Error assigning min: {e}\nlist1: {list1}")
+    if type(list1) == list:
+        for elem in list1:
+            if elem < min and elem > 0.0:
+                min = elem
+    else:
+        min = 0
     return min
 
 
 def get_log2_list(list1, outname):
     """
     list1 is a list where each element is a list
+    out_dict structure
+{
+   "gene":{
+      "ensembl1":{
+         "denominator":[
+            1,
+            2,
+            3,
+            20
+         ],
+         "numerator":[
+            1,
+            2,
+            3,
+            20
+         ]
+      },
+      "ensembl2":{
+         "denominator":[
+            1,
+            2,
+            3,
+            20
+         ],
+         "numerator":[
+            1,
+            2,
+            3,
+            20
+         ]
+      }
+   }
+}
     """
+
     out_dict = {}
     for idx in range(len(list1)):
         ensembl = list1[idx][0]
@@ -41,31 +82,37 @@ def get_log2_list(list1, outname):
         group1_floats = [float(i) for i in group1_fields]
         group2_floats = [float(i) for i in group2_fields]
 
-        group_floats = group1_floats + group2_floats
-
-        min = get_min(group_floats)
-        pseudo_count = min * 0.1 if min else 0.01
-        print(f"Min for {ensembl} / {gene}: {min}, pseudo_count: {pseudo_count}")
-
-        group1_avg = sum(group1_floats) / len(group1_floats)
-        group2_avg = sum(group2_floats) / len(group2_floats)
-
         if gene in out_dict.keys():
-            out_dict[gene]["denominator"].append(group1_avg)
-            out_dict[gene]["numerator"].append(group2_avg)
-            out_dict[gene]["log2"] = math.log2(
-                (sum(out_dict[gene]["numerator"]) + pseudo_count)
-                / (sum(out_dict[gene]["denominator"]) + pseudo_count)
-            )
-        else:
-            log2ratio = math.log2(
-                ((group2_avg + pseudo_count) / (group1_avg + pseudo_count))
-            )
-            out_dict[gene] = {
-                "log2": log2ratio,
-                "numerator": [group2_avg],
-                "denominator": [group1_avg],
+            out_dict[gene][ensembl] = {
+                "denominator": group1_floats,
+                "numerator": group2_floats,
             }
+        else:
+            out_dict[gene] = {
+                ensembl: {"numerator": group2_floats, "denominator": group1_floats,}
+            }
+
+    for k1, v1 in out_dict.items():
+        denominator = []
+        numerator = []
+        for k2 in v1.keys():
+            # print(
+            #     f"denominator: {denominator}, numerator: {numerator}, v1[{k2}]['denominator']: {v1[k2]['denominator']}, v1[{k2}]['numerator']: {v1[k2]['numerator']}"
+            # )
+            denominator += v1[k2]["denominator"]
+            numerator += v1[k2]["numerator"]
+        out_dict[k1]["denominator_tc"] = sum(denominator)
+        out_dict[k1]["numerator_tc"] = sum(numerator)
+        if dynamic_pseudo_count:
+            gene_counts = denominator + numerator
+            min = get_min(gene_counts)
+            pseudo_count = min * 0.1
+            # print(f"min and pseudo_count for gene {k1}: {min}, {pseudo_count}")
+        group1_avg = sum(denominator) / len(denominator)
+        group2_avg = sum(numerator) / len(numerator)
+        log2ratio = math.log2((group2_avg + pseudo_count) / (group1_avg + pseudo_count))
+        v1["log2"] = log2ratio
+
     with open(f"{results_dir}/{outname}_counts.json", "w") as fp:
         fp.write(json.dumps(out_dict))
 
@@ -106,60 +153,65 @@ with open(f"{results_dir}/all_grouped_filtered_normalized.sf", "r") as infile:
     F_WT_KO_outdict = get_log2_list(F_WT_KO_list, "F_WT_KO")
     F_WT_KO_keys = list(F_WT_KO_outdict.keys())
     F_WT_KO_keys.sort()
+    print("Writing log2 file for F_WT_KO")
     with open(f"{results_dir}/log2_F_WT_KO_from_all.txt", "w") as outfile:
-        outfile.write("Gene\tLog2\tDenominator\tNumerator\n")
+        outfile.write("Gene\tLog2\tControl\tTest\n")
         for key in F_WT_KO_keys:
             gene = key
             log2 = F_WT_KO_outdict[key]["log2"]
-            denominator = F_WT_KO_outdict[key]["denominator"]
-            numerator = F_WT_KO_outdict[key]["numerator"]
+            denominator = F_WT_KO_outdict[key]["denominator_tc"]
+            numerator = F_WT_KO_outdict[key]["numerator_tc"]
+
             outfile.write(
-                f"{gene}\t{round(log2, 2)}\t{round(sum(denominator), 3)}\t{round(sum(numerator), 3)}\n"
+                f"{gene}\t{round(log2, 2)}\t{round(denominator, 3)}\t{round(numerator, 3)}\n"
             )
 
     # log2 ratio for M_WT & M_KO
     M_WT_KO_outdict = get_log2_list(M_WT_KO_list, "M_WT_KO")
     M_WT_KO_keys = list(M_WT_KO_outdict.keys())
     M_WT_KO_keys.sort()
+    print("Writing log2 file for M_WT_KO")
     with open(f"{results_dir}/log2_M_WT_KO_from_all.txt", "w") as outfile:
-        outfile.write("Gene\tLog2\tDenominator\tNumerator\n")
+        outfile.write("Gene\tLog2\tControl\tTest\n")
         for key in M_WT_KO_keys:
             gene = key
             log2 = M_WT_KO_outdict[key]["log2"]
-            denominator = M_WT_KO_outdict[key]["denominator"]
-            numerator = M_WT_KO_outdict[key]["numerator"]
+            denominator = M_WT_KO_outdict[key]["denominator_tc"]
+            numerator = M_WT_KO_outdict[key]["numerator_tc"]
             outfile.write(
-                f"{gene}\t{round(log2, 2)}\t{round(sum(denominator), 3)}\t{round(sum(numerator), 3)}\n"
+                f"{gene}\t{round(log2, 2)}\t{round(denominator, 3)}\t{round(numerator, 3)}\n"
             )
 
     # log2 ratio for M_KO & F_KO
     KO_FM_outdict = get_log2_list(KO_FM_list, "KO_FM")
     KO_FM_keys = list(KO_FM_outdict.keys())
     KO_FM_keys.sort()
+    print("Writing log2 file for KO_FM")
     with open(f"{results_dir}/log2_KO_FM_from_all.txt", "w") as outfile:
-        outfile.write("Gene\tLog2\tDenominator\tNumerator\n")
+        outfile.write("Gene\tLog2\tControl\tTest\n")
         # outfile.write("Gene\tLog2\tDenominator\tNumerator\n")
         for key in KO_FM_keys:
             gene = key
             log2 = KO_FM_outdict[key]["log2"]
-            denominator = KO_FM_outdict[key]["denominator"]
-            numerator = KO_FM_outdict[key]["numerator"]
+            denominator = KO_FM_outdict[key]["denominator_tc"]
+            numerator = KO_FM_outdict[key]["numerator_tc"]
             outfile.write(
-                f"{gene}\t{round(log2, 2)}\t{round(sum(denominator), 3)}\t{round(sum(numerator), 3)}\n"
+                f"{gene}\t{round(log2, 2)}\t{round(denominator, 3)}\t{round(numerator, 3)}\n"
             )
 
     # log2 ratio for M_WT & F_WT
     WT_FM_outdict = get_log2_list(WT_FM_list, "WT_FM")
     WT_FM_keys = list(WT_FM_outdict.keys())
     WT_FM_keys.sort()
+    print("Writing log2 file for WT_FM")
     with open(f"{results_dir}/log2_WT_FM_from_all.txt", "w") as outfile:
-        outfile.write("Gene\tLog2\tDenominator\tNumerator\n")
+        outfile.write("Gene\tLog2\tControl\tTest\n")
         # outfile.write("Gene\tLog2\tDenominator\tNumerator\n")
         for key in WT_FM_keys:
             gene = key
             log2 = WT_FM_outdict[key]["log2"]
-            denominator = WT_FM_outdict[key]["denominator"]
-            numerator = WT_FM_outdict[key]["numerator"]
+            denominator = WT_FM_outdict[key]["denominator_tc"]
+            numerator = WT_FM_outdict[key]["numerator_tc"]
             outfile.write(
-                f"{gene}\t{round(log2, 2)}\t{round(sum(denominator), 3)}\t{round(sum(numerator), 3)}\n"
+                f"{gene}\t{round(log2, 2)}\t{round(denominator, 3)}\t{round(numerator, 3)}\n"
             )
